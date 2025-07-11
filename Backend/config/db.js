838f47b0be4a97232ea3bdd383f6c821762ad549
Hacklivly <128738691+hacklivly@users.noreply.github.com>
2025-07-12 00:43:03 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://trendscafe:harshverma@cluster0.undwg8j.mongodb.net/trendscafefood').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://trendscafe:harshverma@cluster0.undwg8j.mongodb.net/trendscafefood?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Connected"));
 }
